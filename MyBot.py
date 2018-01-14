0d2a8b17a2b1fd5d1c965a5317fd20a28804f0d9
ScranchNew <35395613+ScranchNew@users.noreply.github.com>
2018-01-14 15:48:01 +0100

    Improved all kinds of shit

--- MyBot.py
+++ MyBot.py
@@ -32,7 +32,7 @@ while True:
     # Update the map for the new turn and get the latest version
     game_map = game.update_map()
     
-    if first_turn == 1:
+    if first_turn == True:
         centre_place = Position(0,0)
         centre_place.x = game_map.get_me().all_ships()[0].x
         centre_place.y = game_map.get_me().all_ships()[0].y
@@ -72,19 +72,18 @@ while True:
     logging.info("Listed the planets")
     
     # Find the centre of my empire
-    if len(my_planets) > 0:
-        for planet in my_planets:
-            centre_place.x += planet.x
-            centre_place.y += planet.y
-        
-        centre_place.x /= len(my_planets)
-        centre_place.y /= len(my_planets)
-    else:
-        centre_place.x = game_map.get_me().all_ships()[0].x
-        centre_place.y = game_map.get_me().all_ships()[0].y
+    '''centre_place.x = 0
+    centre_place.y = 0
+    
+    for ship in game_map.get_me().all_ships():
+        centre_place.x += ship.x
+        centre_place.y += ship.y
+    
+    centre_place.x /= len(game_map.get_me().all_ships())
+    centre_place.y /= len(game_map.get_me().all_ships())
         
     logging.info("New Centre.x: " + str(centre_place.x))
-    logging.info("New Centre.y: " + str(centre_place.y))
+    logging.info("New Centre.y: " + str(centre_place.y))'''
     
     # Depending on the distribution I change the gamestate
     
@@ -98,131 +97,156 @@ while True:
     # Rules for early game (state 0)
     if game_state == 0:
         active_ships = []
+        active = False
         for ship in game_map.get_me().all_ships():
             if str(ship.docking_status) == 'DockingStatus.UNDOCKED':
                 active_ships.append(ship)
+                active = True
             continue
         
         logging.info("Listed active ships")
+        logging.info("active ships: " + str(active_ships))
         
-        closest_ship = active_ships[0]
+        mining_num = int(len(active_ships)*2/3)
+        if mining_num < 2:
+            mining_num = len(active_ships)
+        war_num = len(active_ships) - mining_num
         
-        # Navigate ships to not already filled planets
-        open_places = 0
-        targets = []
-        for planet in my_planets_not_full:
-            logging.info("Not filled Planet: " + str(planet))
-            logging.info("Ships - Spots: " + str(len(active_ships) - open_places))
-            logging.info(" Docking spots: " + str(planet.num_docking_spots - len(planet.all_docked_ships())))
-            if (len(active_ships) - open_places > 0):
+        war_ships = active_ships[mining_num:len(active_ships)]
+        active_ships = active_ships[0:mining_num]
+        
+        if active:
+            closest_ship = active_ships[0]
+            
+            # Navigate ships to not already filled planets
+            ships_left = len(active_ships)
+            targets = []
+            
+            for planet in my_planets_not_full:
+                logging.info("Not filled Planet: " + str(planet))
+                logging.info("Ships left: " + str(ships_left))
+                logging.info("Docking spots: " + str(planet.num_docking_spots - len(planet.all_docked_ships())))
+                
                 targets.append(planet)
-                open_places += planet.num_docking_spots - len(planet.all_docked_ships())
-                continue
+                if (ships_left <= planet.num_docking_spots - len(planet.all_docked_ships())):
+                    break
+                ships_left -= planet.num_docking_spots - len(planet.all_docked_ships())
+                
+            targets.reverse()
+            logging.info("Not filled Targets" + str(targets))
+            first_target = True
+            for target in targets:
+                docking_allowed = True
+                logging.info("Current target: " + str(target) + " Docking spots: " + str(target.num_docking_spots))
+                logging.info("range: " + str(target.num_docking_spots - len(target.all_docked_ships())))
+                for places in range(target.num_docking_spots - len(target.all_docked_ships())):
+                    if first_target and (places >= ships_left):
+                        logging.info("Breaking at " + str(ships_left))
+                        first_target = False
+                        break
+                    logging.info("searching for ship " + str(places))
+                    min_dist = 10000
+                    logging.info("Active ships: " + str(active_ships))
+                    for ship in active_ships:
+                        dist_to_tgt = ship.calculate_distance_between(target)
+                        if dist_to_tgt < min_dist:
+                            min_dist = dist_to_tgt
+                            closest_ship = ship
+                    logging.info("Closest ship: " + str(closest_ship))
+                    active_ships.remove(closest_ship)
+                    # If we can dock, let's (try to) dock. If two ships try to dock at once, neither will be able to.
+                    if closest_ship.can_dock(target):
+                        # We add the command by appending it to the command_queue
+                        if docking_allowed:
+                            docking_allowed = False
+                        logging.info("Nav command: " + str(closest_ship.dock(target)))
+                        command_queue.append(closest_ship.dock(target))
+                    # Otherwise move towards it
+                    else:
+                        navigate_command = closest_ship.navigate(closest_ship.closest_point_to(target), game_map, max_corrections=180, angular_step=5, speed=int(hlt.constants.MAX_SPEED), ignore_ships=False)
+                        logging.info("Nav command: " + str(navigate_command))
+                        # If we can
+                        if navigate_command:
+                            command_queue.append(navigate_command)
+                    if len(active_ships) < 1:
+                        break
+            
+            logging.info("Navigated to not filled")
+            
+            # Navigate ships to empty planets
+            ships_left = len(active_ships)
+            targets = []
+            
+            for planet in empty_planets:
+                logging.info("Not filled Planet: " + str(planet))
+                logging.info("Ships left: " + str(ships_left))
+                logging.info("Docking spots: " + str(planet.num_docking_spots))
+                
+                targets.append(planet)
+                if (ships_left < planet.num_docking_spots):
+                    break
+                ships_left -= planet.num_docking_spots
+            
+            targets.reverse()
+            logging.info("Empty Targets: " + str(targets))
+            first_target = True
+            for target in targets:
+                docking_allowed = True
+                logging.info("Current target: " + str(target) + " Docking spots: " + str(target.num_docking_spots))
+                logging.info("range: " + str(target.num_docking_spots))
+                for places in range(target.num_docking_spots):
+                    if first_target and (places >= ships_left):
+                        logging.info("Breaking at " + str(ships_left))
+                        first_target = False
+                        break
+                    logging.info("searching for ship " + str(places))
+                    min_dist = 10000
+                    logging.info("Active ships: " + str(active_ships))
+                    for ship in active_ships:
+                        dist_to_tgt = ship.calculate_distance_between(target)
+                        if dist_to_tgt < min_dist:
+                            min_dist = dist_to_tgt
+                            closest_ship = ship
+                    logging.info("Closest ship: " + str(closest_ship))
+                    active_ships.remove(closest_ship)
+                    # If we can dock, let's (try to) dock. If two ships try to dock at once, neither will be able to.
+                    if closest_ship.can_dock(target):
+                        # We add the command by appending it to the command_queue
+                        if docking_allowed:
+                            docking_allowed = False
+                            logging.info("Nav command: " + str(closest_ship.dock(target)))
+                            command_queue.append(closest_ship.dock(target))
+                    # Otherwise move towards it
+                    else:
+                        navigate_command = closest_ship.navigate(closest_ship.closest_point_to(target), game_map, max_corrections=180, angular_step=5, speed=int(hlt.constants.MAX_SPEED), ignore_ships=False)
+                        logging.info("Nav command: " + str(navigate_command))
+                        # If we can
+                        if navigate_command:
+                            command_queue.append(navigate_command)
+                    if len(active_ships) < 1:
+                        break
+                
+            logging.info("Navigated to empty")
+            
+            # Navigate ships to enemy planets
+            active_ships = war_ships + active_ships
+            
+            if len(enemy_planets) >= 2:
+                target_num = 2
             else:
-                break
-        targets.reverse()
-        logging.info("Not filled Targets" + str(targets))
-        for target in targets:
-            logging.info("Current target: " + str(target))
-            for places in range(1, planet.num_docking_spots - len(planet.all_docked_ships())):
-                min_dist = 10000
-                logging.info("Active ships: " + str(active_ships))
-                for ship in active_ships:
-                    dist_to_tgt = ship.calculate_distance_between(target)
-                    if dist_to_tgt < min_dist:
-                        min_dist = dist_to_tgt
-                        closest_ship = ship
-                logging.info("Closest ship: " + str(closest_ship))
-                active_ships.remove(closest_ship)
-                # If we can dock, let's (try to) dock. If two ships try to dock at once, neither will be able to.
-                if closest_ship.can_dock(target):
-                    # We add the command by appending it to the command_queue
-                    logging.info("Nav command: " + str(closest_ship.dock(target)))
-                    command_queue.append(closest_ship.dock(target))
-                # Otherwise move towards it
-                else:
-                    navigate_command = closest_ship.navigate(closest_ship.closest_point_to(target), game_map, max_corrections=180, angular_step=3, speed=int(hlt.constants.MAX_SPEED*5/7), ignore_ships=False)
+                target_num = len(enemy_planets)
+            targets = []
+            if target_num > 0:
+                targets = enemy_planets
+                logging.info("Enemy Targets" + str(targets))
+                for ship_num in range(len(active_ships)):
+                    target_ship = targets[ship_num % target_num].all_docked_ships()[0]
+                    navigate_command = active_ships[ship_num].navigate(active_ships[ship_num].closest_point_to(target_ship), game_map, max_corrections=180, angular_step=5, speed=int(hlt.constants.MAX_SPEED), ignore_ships=True)
                     logging.info("Nav command: " + str(navigate_command))
-                    # If we can
                     if navigate_command:
                         command_queue.append(navigate_command)
-                if len(active_ships) < 1:
-                    break
-        
-        logging.info("Navigated to not filled")
-        
-        # Navigate ships to empty planets
-        open_places = 0
-        targets = []
-        for planet in empty_planets:
-            logging.info("Empty Planet: " + str(planet))
-            logging.info("Ships - Spots: " + str(len(active_ships) - open_places))
-            logging.info(" Docking spots: " + str(planet.num_docking_spots))
-            if (len(active_ships) - open_places > 0):
-                targets.append(planet)
-                open_places += planet.num_docking_spots
-            else:
-                break
-        targets.reverse()
-        logging.info("Empty Targets: " + str(targets) + " Docking spots: " + str(planet.num_docking_spots - len(planet.all_docked_ships())))
-        for target in targets:
-            logging.info("Current target: " + str(target))
-            for places in range(1, planet.num_docking_spots):
-                min_dist = 10000
-                logging.info("Active ships: " + str(active_ships))
-                for ship in active_ships:
-                    dist_to_tgt = ship.calculate_distance_between(target)
-                    if dist_to_tgt < min_dist:
-                        min_dist = dist_to_tgt
-                        closest_ship = ship
-                logging.info("Closest ship: " + str(closest_ship))
-                active_ships.remove(closest_ship)
-                # If we can dock, let's (try to) dock. If two ships try to dock at once, neither will be able to.
-                if closest_ship.can_dock(target):
-                    # We add the command by appending it to the command_queue
-                    logging.info("Nav command: " + str(closest_ship.dock(target)))
-                    command_queue.append(closest_ship.dock(target))
-                # Otherwise move towards it
-                else:
-                    navigate_command = closest_ship.navigate(closest_ship.closest_point_to(target), game_map, max_corrections=180, angular_step=3, speed=int(hlt.constants.MAX_SPEED*5/7), ignore_ships=False)
-                    logging.info("Nav command: " + str(navigate_command))
-                    # If we can
-                    if navigate_command:
-                        command_queue.append(navigate_command)
-                if len(active_ships) < 1:
-                    break
-        
-        logging.info("Navigated to empty")
-        
-        # Navigate ships to enemy planets
-        targets = []
-        ships_per_target = 5
-        for to_kill in range(0, int(len(active_ships)/ships_per_target) - 1):
-            targets.insert(0, enemy_planets[to_kill])
-        logging.info("Enemy Targets" + str(targets))
-        for target in targets:
-            logging.info("Current target: " + str(target) + " Docking spots: " + str(planet.num_docking_spots - len(planet.all_docked_ships())))
-            for num in range(1, ships_per_target):
-                min_dist = 10000
-                logging.info("Active ships: " + str(active_ships))
-                for ship in active_ships:
-                    dist_to_tgt = ship.calculate_distance_between(target)
-                    if dist_to_tgt < min_dist:
-                        min_dist = dist_to_tgt
-                        closest_ship = ship
-                logging.info("Closest ship: " + str(closest_ship))
-                active_ships.remove(closest_ship)
-                
-                target_ship = target.all_docked_ships()[0]
-                
-                navigate_command = closest_ship.navigate(closest_ship.closest_point_to(target_ship), game_map, max_corrections=180, angular_step=3, speed=int(hlt.constants.MAX_SPEED), ignore_ships=True)
-                logging.info("Nav command: " + str(navigate_command))
-                if navigate_command:
-                    command_queue.append(navigate_command)
-                if len(active_ships) < 1:
-                    break
-         
-        logging.info("Navigated to the enemy")
+             
+            logging.info("Navigated to the enemy")
         
     # Rules for mid game (state 1)
     elif game_state == 1:
