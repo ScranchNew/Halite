28f671e272d7611e8afe84a7beb3b22948507217
ScranchNew <35395613+ScranchNew@users.noreply.github.com>
2018-01-14 02:22:51 +0100

    Redid all the ship targeting algorythms

--- MyBot.py
+++ MyBot.py
@@ -1,12 +1,10 @@
 """
 Welcome to your first Halite-II bot!
-
 This bot's name is Settler. It's purpose is simple (don't expect it to win complex games :) ):
 1. Initialize game
 2. If a ship is not docked and there are unowned planets
 2.a. Try to Dock in the planet if close enough
 2.b If not, go towards the planet
-
 Note: Please do not place print statements here as they are used to communicate with the Halite engine. If you need
 to log anything use the logging module.
 """
@@ -21,6 +19,19 @@ game = hlt.Game("Settler")
 # Then we print our start message to the logs
 logging.info("Starting my Settler bot!")
 
+class game_state_list(Enum):
+        EARLY = 0
+        MID = 1
+        LATE = 2
+
+game_state = EARLY
+
+game_map = game.update_map()
+
+centre_place = Position(0,0)
+centre_place.x = map.get_me().all_ships()[0].x
+centre_place.y = map.get_me().all_ships()[0].y
+
 while True:
     # TURN START
     # Update the map for the new turn and get the latest version
@@ -28,46 +39,137 @@ while True:
 
     # Here we define the set of commands to be sent to the Halite engine at the end of the turn
     command_queue = []
-    # For every ship that I control
-    for ship in game_map.get_me().all_ships():
-        # If the ship is docked
-        if ship.docking_status != ship.DockingStatus.UNDOCKED:
-            # Skip this ship
-            continue
-
-        # For each planet in the game (only non-destroyed planets are included)
-        for planet in game_map.all_planets():
-            # If the planet is owned
-            if planet.is_owned():
-                # Skip this planet
+    
+    # Here I count and categorize all the planets in the game
+    my_planets = []
+    my_planets_not_full = []
+    empty_planets = []
+    enemy_planets = []
+    
+    for entity in game_map.nearby_entities_by_distance(centre_place):
+        if entity.__name__ == 'Planet'
+            if entity.is_owned() == false:
+                empty_planets.append(entity)
                 continue
-
-            # If we can dock, let's (try to) dock. If two ships try to dock at once, neither will be able to.
-            if ship.can_dock(planet):
-                # We add the command by appending it to the command_queue
-                command_queue.append(ship.dock(planet))
+            elif entity.owner == map.get_me():
+                if entity.is_full() == false:
+                    my_planets_not_full.append(entity)
+                my_planets.append(entity)
+                continue
             else:
-                # If we can't dock, we move towards the closest empty point near this planet (by using closest_point_to)
-                # with constant speed. Don't worry about pathfinding for now, as the command will do it for you.
-                # We run this navigate command each turn until we arrive to get the latest move.
-                # Here we move at half our maximum speed to better control the ships
-                # In order to execute faster we also choose to ignore ship collision calculations during navigation.
-                # This will mean that you have a higher probability of crashing into ships, but it also means you will
-                # make move decisions much quicker. As your skill progresses and your moves turn more optimal you may
-                # wish to turn that option off.
-                navigate_command = ship.navigate(
-                    ship.closest_point_to(planet),
-                    game_map,
-                    speed=int(hlt.constants.MAX_SPEED/2),
-                    ignore_ships=True)
-                # If the move is possible, add it to the command_queue (if there are too many obstacles on the way
-                # or we are trapped (or we reached our destination!), navigate_command will return null;
-                # don't fret though, we can run the command again the next turn)
+                enemy_planets.append(entity)
+        continue
+    
+    # Find the centre of my empire
+    if len(my_planets) > 0:
+        for planet in my_planets:
+            centre_place.x += planet.x
+            centre_place.y += planet.y
+        
+        centre_place.x /= len(my_planets)
+        centre_place.y /= len(my_planets)
+    else:
+        centre_place.x = map.get_me().all_ships()[0].x
+        centre_place.y = map.get_me().all_ships()[0].y
+    
+    # Depending on the distribution I change the gamestate
+    
+    # Midgame (Gamestate 1) starts when there are less empty than occupied planets
+    if (len(my_planets) + len(enemy_planets)) > len(empty_planets):
+        #game_state = MID
+    # Lategame (Gamestate 2) starts when there are no empty planets left
+    elif (len(empty_planets) == 0):
+        #game_state = LATE
+    
+    # Rules for early game (state 0)
+    if game_state = EARLY:
+        active_ships = []
+        for ship in map.get_me().all_ships():
+            if ship.docking_status = 0:
+                active_ships.append(ship)
+            continue
+        
+        # Navigate ships to not already filled planets
+        open_places = 0
+        targets = []
+        for planet in my_planets_not_full:
+            if (len(active_ships) - open_places > 0):
+                targets.append(planet)
+                open_places += planet.num_docking_spots - len(planet.all_docked_ships())
+                continue
+            else:
+                break
+        targets.reverse()
+        for target in targets:
+            for places in range(1, planet.num_docking_spots - len(planet.all_docked_ships())):
+                min_dist = 10000
+                closest_ship
+                for ship in active_ships:
+                    dist = ship.calculate_distance_between(target)
+                    if dist < min_dist:
+                        min_dist = dist
+                        closest_ship = ship
+                active_ships.remove(closest_ship)
+                navigate_command = ship.navigate(ship.closest_point_to(target), game_map, max_corrections=180, angular_step=3, speed=int(hlt.constants.MAX_SPEED*5/7), ignore_ships=False)
                 if navigate_command:
                     command_queue.append(navigate_command)
-            break
-
-    # Send our set of commands to the Halite engine for this turn
+                continue
+        
+        # Navigate ships to empty planets
+        open_places = 0
+        targets = []
+        for planet in empty_planets:
+            if (len(active_ships) - open_places > 0):
+                targets.append(planet)
+                open_places += planet.num_docking_spots)
+                continue
+            else:
+                break
+        targets.reverse()
+        for target in targets:
+            for places in range(1, planet.num_docking_spots):
+                min_dist = 10000
+                closest_ship
+                for ship in active_ships:
+                    dist = ship.calculate_distance_between(target)
+                    if dist < min_dist:
+                        min_dist = dist
+                        closest_ship = ship
+                active_ships.remove(closest_ship)
+                navigate_command = ship.navigate(ship.closest_point_to(target), game_map, max_corrections=180, angular_step=3, speed=int(hlt.constants.MAX_SPEED*5/7), ignore_ships=False)
+                if navigate_command:
+                    command_queue.append(navigate_command)
+                continue
+        
+        # Navigate ships to enemy planets
+        targets = []
+        ships_per_target = 5
+        for to_kill in range(0, floor(len(active_ships)/ships_per_target) - 1):
+            targets.insert(0, enemy_planets[to_kill])
+        for target in targets:
+            for num in range(1, ships_per_target):
+                min_dist = 10000
+                closest_ship
+                for ship in active_ships:
+                    dist = ship.calculate_distance_between(target)
+                    if dist < min_dist:
+                        min_dist = dist
+                        closest_ship = ship
+                active_ships.remove(closest_ship)
+                
+                target_ship = target.all_docked_ships()[0]
+                
+                navigate_command = ship.navigate(ship.closest_point_to(target_ship), game_map, max_corrections=180, angular_step=3, speed=int(hlt.constants.MAX_SPEED), ignore_ships=True)
+                if navigate_command:
+                    command_queue.append(navigate_command)
+                continue
+                
+    # Rules for mid game (state 1)
+    elif game_state = MID:
+        
+    # Rules for the late game (state 2)
+    else:
+        
     game.send_command_queue(command_queue)
     # TURN END
 # GAME END
